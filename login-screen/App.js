c73378d3c878812a27a59659387ce091021d9eef
Chuks Ez <chukwudiezichi@gmail.com>
2021-10-18 23:25:55 +0100

    upgrade rnative version

--- login-screen/App.js
+++ login-screen/App.js
@@ -1,21 +1,7 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
 
-import LoginScreen from './src/login/login.js'
+import LoginScreen from "./src/login/login.js";
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <LoginScreen />
-    );
-  }
+export default function App() {
+  return <LoginScreen />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- login-screen/src/login/login.js
+++ login-screen/src/login/login.js
@@ -1,62 +1,45 @@
-import React, { Component } from "react";
+import React from "react";
 
 import styles from "./style";
-import {Keyboard, Text, View, TextInput, TouchableWithoutFeedback, Alert, KeyboardAvoidingView} from 'react-native';
-import { Button } from 'react-native-elements';
+import { Alert, Image, Keyboard, KeyboardAvoidingView, Text, TextInput, TouchableWithoutFeedback, Pressable, View } from "react-native";
+import { Button, SocialIcon } from "react-native-elements";
+import * as Facebook from "expo-facebook";
 
-const appId = "1047121222092614"
+const appId = "1047121222092614";
 
-export default class LoginScreen extends Component {
+export default function LoginScreen() {
+  const onLoginPress = () => {};
 
-  render() {
-    return (
-      <KeyboardAvoidingView style={styles.containerView} behavior="padding">
+  const onFbLoginPress = async () => {
+    try {
+      await Facebook.initializeAsync({
+        appId,
+      });
+      const { type, token } = await Facebook.logInWithReadPermissionsAsync({
+        permissions: ["public_profile", "email"],
+      });
+      if (type === "success") {
+        const response = await fetch(`https://graph.facebook.com/me?access_token=${token}`);
+        Alert.alert("Logged in!", `Hi ${(await response.json()).name}!`);
+      }
+    } catch ({ message }) {
+      Alert.alert(`Facebook Login Error: ${message}`);
+    }
+  };
 
+  return (
+    <KeyboardAvoidingView style={styles.containerView} behavior="padding">
       <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
         <View style={styles.loginScreenContainer}>
           <View style={styles.loginFormView}>
-          <Text style={styles.logoText}>Instamobile</Text>
+            <Text style={styles.logoText}>Instamobile</Text>
             <TextInput placeholder="Username" placeholderColor="#c4c3cb" style={styles.loginFormTextInput} />
-            <TextInput placeholder="Password" placeholderColor="#c4c3cb" style={styles.loginFormTextInput} secureTextEntry={true}/>
-            <Button
-              buttonStyle={styles.loginButton}
-              onPress={() => this.onLoginPress()}
-              title="Login"
-            />
-            <Button
-              buttonStyle={styles.fbLoginButton}
-              onPress={() => this.onFbLoginPress()}
-              title="Login with Facebook"
-              color="#3897f1"
-            />
+            <TextInput placeholder="Password" placeholderColor="#c4c3cb" style={styles.loginFormTextInput} secureTextEntry={true} />
+            <Button buttonStyle={styles.loginButton} onPress={() => onLoginPress()} title="Login" />
+            <Button containerStyle={styles.fbLoginButton} type='clear' onPress={() => onFbLoginPress()} title="Login With Facebook" />
           </View>
         </View>
       </TouchableWithoutFeedback>
-      </KeyboardAvoidingView>
-    );
-  }
-
-  componentDidMount() {
-  }
-
-  componentWillUnmount() {
-  }
-
-  onLoginPress() {
-
-  }
-
-  async onFbLoginPress() {
-    const { type, token } = await Expo.Facebook.logInWithReadPermissionsAsync(appId, {
-      permissions: ['public_profile', 'email'],
-    });
-    if (type === 'success') {
-      const response = await fetch(
-        `https://graph.facebook.com/me?access_token=${token}`);
-      Alert.alert(
-        'Logged in!',
-        `Hi ${(await response.json()).name}!`,
-      );
-    }
-  }
+    </KeyboardAvoidingView>
+  );
 }
