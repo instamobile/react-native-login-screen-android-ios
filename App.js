f1cefa2a62195b776e283b921ffaf3faf9d3ee58
Florian Marcu <florian.mrc@gmail.com>
2023-07-11 16:42:28 -0700

    Upgrade to latest RN

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 import React from "react";
-
 import LoginScreen from "./src/login/login.js";
 
 export default function App() {
--- src/login/login.js
+++ src/login/login.js
@@ -1,8 +1,16 @@
 import React from "react";
 
 import styles from "./style";
-import { Alert, Keyboard, KeyboardAvoidingView, Text, TextInput, TouchableWithoutFeedback, View } from "react-native";
-import { Button } from "react-native-elements";
+import {
+  Alert,
+  Keyboard,
+  KeyboardAvoidingView,
+  Text,
+  TextInput,
+  TouchableWithoutFeedback,
+  View,
+} from "react-native";
+import { Button, SocialIcon } from "react-native-elements";
 import * as Facebook from "expo-facebook";
 
 const appId = "1047121222092614";
@@ -11,21 +19,25 @@ export default function LoginScreen() {
   const onLoginPress = () => {};
 
   const onFbLoginPress = async () => {
-    try {
-      await Facebook.initializeAsync({
-        appId,
-      });
-      const { type, token } = await Facebook.logInWithReadPermissionsAsync({
-        permissions: ["public_profile", "email"],
-      });
-      if (type === "success") {
-        const response = await fetch(`https://graph.facebook.com/me?access_token=${token}`);
-        Alert.alert("Logged in!", `Hi ${(await response.json()).name}!`);
-      }
-    } catch ({ message }) {
-      Alert.alert(`Facebook Login Error: ${message}`);
-      console.log(message)
-    }
+    Alert.alert(
+      `Please use our React Native Starer Kit instead. You can download it for free at https://instamobile.io`
+    );
+    // try {
+    //   await Facebook.initializeAsync({
+    //     appId,
+    //   });
+    //   const { type, token } = await Facebook.logInWithReadPermissionsAsync({
+    //     permissions: ["public_profile", "email"],
+    //   });
+    //   if (type === "success") {
+    //     const response = await fetch(
+    //       `https://graph.facebook.com/me?access_token=${token}`
+    //     );
+    //     Alert.alert("Logged in!", `Hi ${(await response.json()).name}!`);
+    //   }
+    // } catch ({ message }) {
+    //   Alert.alert(`Facebook Login Error: ${message}`);
+    // }
   };
 
   return (
@@ -34,10 +46,28 @@ export default function LoginScreen() {
         <View style={styles.loginScreenContainer}>
           <View style={styles.loginFormView}>
             <Text style={styles.logoText}>Instamobile</Text>
-            <TextInput placeholder="Username" placeholderColor="#c4c3cb" style={styles.loginFormTextInput} />
-            <TextInput placeholder="Password" placeholderColor="#c4c3cb" style={styles.loginFormTextInput} secureTextEntry={true} />
-            <Button buttonStyle={styles.loginButton} onPress={() => onLoginPress()} title="Login" />
-            <Button containerStyle={styles.fbLoginButton} type='clear' onPress={() => onFbLoginPress()} title="Login With Facebook" />
+            <TextInput
+              placeholder="Username"
+              placeholderColor="#c4c3cb"
+              style={styles.loginFormTextInput}
+            />
+            <TextInput
+              placeholder="Password"
+              placeholderColor="#c4c3cb"
+              style={styles.loginFormTextInput}
+              secureTextEntry={true}
+            />
+            <Button
+              buttonStyle={styles.loginButton}
+              onPress={() => onLoginPress()}
+              title="Login"
+            />
+            <Button
+              containerStyle={styles.fbLoginButton}
+              type="clear"
+              onPress={() => onFbLoginPress()}
+              title="Login With Facebook"
+            />
           </View>
         </View>
       </TouchableWithoutFeedback>
